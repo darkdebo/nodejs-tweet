982f3b7e0a64a07ab6f248b754228155bf8e820a
Debojyoti Chakraborty <50295688+darkdebo@users.noreply.github.com>
2020-03-10 11:42:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,14 +11,14 @@
 
 module.exports = {
   twitter: {
-    consumer_key: "eqS99PFooAF7KulehNv7NLP36",
-    consumer_secret: "vwPH96APug5U0K3LWNK1unwoASqBy7G63lXzOPnweix4dFcnYO",
-    access_token: "729923589978251266-cDPAFDCGGoNaY6kVUkpTqt3Q9vysv3C",
-    access_token_secret: "g5KTz7lIHdmGcnuzghwB70NiTb6qZRA6b4v231i3LIr8Y"
+  consumer_key:         'OmpBWs8mHKiIFDbwJGke7T0uE',
+  consumer_secret:      'NBMddOT4LOmAaE4iKIjSMV1VyqTlWUY7TBgOYkHCQufyV8KdW5',
+  access_token:         '1175597608124862466-dxoZkX9mRcqOT3kd4SoF4AjmWZOw0W',
+  access_token_secret:  'ViY6P0Qnhb586rrQtbaYzbKpBi37iCnIoYvJi5G4hbWPo' 
   },
   twitterConfig: {
     queryString:
-      "#Nodejs, #Angular, #Reactjs, #ionicframework, #ReactNative, #es6"
+      "#Nodejs, #Angular, #Reactjs, #ionicframework, #ReactNative, #es6,#100daysofcode"
     // blackList:
     //   "reactdom,_adeeb,subjecttoreview,stacktrender,adaltarecruit,heroes_bot,_30days30sites,droidscott,scottbottest"
   },
